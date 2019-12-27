88469759cf194d130d95f24cedf2768e3cd8d78d
rachelm123 <mollarachel2@gmail.com>
2019-12-27 11:12:47 +0200

    add score

--- src/api/game.js
+++ src/api/game.js
@@ -1,61 +1,48 @@
 const board = require('./board')
 const player = require('./player')
 
-
 class Game{
   constructor(){
-    this.board = new board();
+    this.board = null;
     this.player1 = new player();
     this.player2 = new player();
     this.currentPlayer = this.player1;
   }
 
   setBoard(rows, columns){
-    this.board.init(rows, columns);
+    this.board = new board();
+    this.board.init(parseInt(rows), parseInt(columns));
   }
 
-  setPlayers(){
-    this.player1.init("#FFDC04");
-    this.player2.init("#CB4335");
+  setPlayers(numOfPlayers){
+    this.player1.init("#FFDC04", this.player1.numberOfWins);
+    this.player2.init("#CB4335", this.player2.numberOfWins);
   }
 
   move(numOfCol){
-    this.board.move(numOfCol, this.currentPlayer.color);
-    return this.board.checkEndGame();
-    // switchUser()
+    if (this.board.move(numOfCol, this.currentPlayer.color)){
+      if (this.board.checkEndGame()){
+        this.currentPlayer.numberOfWins++;
+        return(this.currentPlayer)
+      }
+      this.switchUser();
+      return true
+    }
+    return false;
   }
 
+  
+  switchUser(){
+    if (this.currentPlayer == this.player1){
+      this.currentPlayer = this.player2;
+    }else{
+      this.currentPlayer = this.player1;
+    }
+    console.log(this.currentPlayer);
+    return this.currentPlayer;
+  }
 
 }
 
-const game = new Game();
-
-
-
-console.log(game)
-console.log(game.setBoard(4,5))
-console.log(game.setPlayers())
-// console.log(game.move(1))
-// console.log(game.move(1))
-// console.log(game.move(1))
-// console.log(game.move(1))
-// console.log(game.move(2))
-// console.log(game.move(2))
-// console.log(game.move(2))
-// console.log(game.move(2))
-// console.log(game.move(3))
-// console.log(game.move(3))
-// console.log(game.move(3))
-// console.log(game.move(3))
-// console.log(game.move(4))
-// console.log(game.move(4))
-// console.log(game.move(4))
-// console.log(game.move(4))
-// console.log(game.move(5))
-// console.log(game.move(5))
-// console.log(game.move(5))
-// console.log(game.move(5))
-// console.log(game.move(5))
-
-console.log(game)
+module.exports = Game;
 
